7733262a813be33e59eaae3faf411ff1e06cac00
Heyho17 <117260932+Heyho17@users.noreply.github.com>
2022-11-04 03:25:58 +0700

    Add files via upload

---
+++ Data Software & Bypas.py
@@ -0,0 +1,126 @@
+
+
+
+                      
+       Bypas & Software account
+----------------------REDMI-------------------------
+ _MIUI.11_
+<MENU> 
+               <PILIH BAHASA>
+                -Indonesia √
+                 -english (united kingdom)
+                -azarbaycan
+                 -melayu
+                 -bosanski
+                 -catalá
+                 -cestina
+                 -dansk
+                                                               <NEXT>✔
+-----------------PASANG SIM CARD----------------------
+SIM1 : √
+SIM2 : √
+<BEBAS PAKAI KARTU BERAPAPUN>                                                                  
+----------------<PILIH LOKASI>---------------
+-Indonesia √
+-Afghanistan
+-afrika selatan
+-Albania
+-aljazair
+-amerika srikat
+-andorra                                                  
+                                                          <NEXT> ✔
+-----------------------JARINGAN WIFI---------------------
+- Sambungkan Kepada WiFi Yg Aktif √
+                                                          <NEXT> ✔
+--------------SYARAT DAN KETENTUAN-----------  [√]
+                                                           <NEXT> ✔
+-------------SALIN APLIKASI & DATA-------------
+
+-Jangan Salin √
+                                                             <NEXT> ✔
+----------------------GAMBAR POLA-----------------------
+                      
+                   °            °            °  
+        
+                   °            °            °       <-----[LUPA]
+      
+                   °            °            °            KLIK ⬇️
+
+<Gunakan Akun Google Saya> ✅
+
+------------------------GOOGLE-----------------------
+                VERIFIKASI AKUN ANDA
+              Menggunakan Akun Google
+
+Email : BAGI YG INGAT SAJA
+Password : BAGI YG INGAT SAJA
+
+Bagi Yg lupa Akun Google nya ? Silahkan ikuti Cara DI ⬇️
+
+-----------KEMBALI KE JARINGAN WIFI---------
+- SAMBUNGKAN KEPADA WIFI AKTIF ✔
+- KEMUDIAN KLIK YG DI
+                     ⬇️
+<TAMBAHKAN JARINGAN> ✔
+[SSID                                www.YouTube.com]
+                                                        ⬆️
+                                              <TEKAN LAMA>
+           
+ 
+         > KEMUDIAN<
+       <KLIK GAMBAR>
+           SEPERTI DI ⬇️
+                         ->  📤 <-
+                                             
+                                  <DAN PILIH PESAN>
+                                      >KEMUDIAN ⬇️
+-------------------------KIRIM SMS----------------------
+
+Www.YouTube.com ✔
+>Kirim kepada < ⬇️
+                           (234)
+
+          <TEKAN PESAN YG DI KIRIM>
+            Pilih <TELUSURI YOUTUBE>
+
+------------------------YOUTUBE------------------------
+
+[OK] ✔
+                 <KEMUDIAN KLIK>⬇️
+                         >👤AKUN<
+                           <STELAN>
+                           <HISTORY & PRIVASI>
+         <PERSYARATAN LAYANAN YOUTUBE>
+
+<Stuju & lanjutkan> ✔
+
+<Browser>⬇️
+[ vnrom Bypass ]
+
+-KLIK NO 1
+---------------------Bypas Android-------------------
+
+
+Install >ANDROID_8-9_GAM.apk
+Install >FRP_Bypass.apk
+--------------------------------------------------------
+-FRP_Bypass.apk (OPEN)
+-ANDROID_8-9_GAM.apk (CLOSED)
+                                ⬇️
+ <KLIK TITIK 3 DI POJOK KANAN ATAS>
+<MASUK BROWSER> ✔
+[OK] ✔
+                         >GOOGLE<
+       Login Dengan Akun Google Anda
+                                  👤
+
+              Email : Bagi Yg punya Akun Tumbal
+      Password : Bagi Yg Punya Akun Tumbal
+               
+>BAGI YG BELUM PUNYA AKUN TUMBAL<
+>langsung Saja KLIK ⬇️
+                             <BUAT AKUN>
+
+
+----------------------ALHAMDULILAH-----------------
+ >By Andriyan🐣                       
